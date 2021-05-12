5507c0400e15a5a2110abbd7b09c935770d9390c
slli4 <55905473+slli4@users.noreply.github.com>
2021-05-12 17:47:40 +0800

    run the project

---
+++ src/Component/Heading/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Heading() {
+  return (
+    <div className="Heading">
+      <button className="Heading-button" type="button">aaaa</button>
+    </div>
+
+  );
+}
