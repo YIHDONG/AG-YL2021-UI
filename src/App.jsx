93737a9938659b69cec8961db64ce47ada197361
Samer Adra <adras@student.unimelb.edu.au>
2021-05-24 19:43:16 +1000

    Learning Page Version 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,97 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import api from './api';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [courses, setCoursesList] = useState('No courses yet!');
-  const [loading, setLoading] = useState(false);
+const LearningPageText = styled.p`
+/* body */
+font-family: Lato;
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
 
-  const getCoursesList = () => {
+/* text */
+color: #2B1953;
+`;
+
+const LearningPageImg = styled.img`
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px 0 0 10px;
+  background: #FFFFFF;
+  width: 200 px;
+  height: 200 px;
+  max-height: 400px;
+  max-width: 400px;
+`;
+
+const AnimationVideo = styled.iframe`
+  position: relative;
+  /* overflow: hidden;
+  padding-top: 56.25%; */
+`;
+
+// const sectionNumber = 1;
+
+function sectionRendering(section) {
+  if (section.type === 'image') {
+    return (
+      <LearningPageImg src={section.content} alt="logo" />
+    );
+  }
+  if (section.type === 'text') {
+    return (
+      <LearningPageText>{section.content}</LearningPageText>
+    );
+  }
+  if (section.type === 'animation') {
+    return (
+      <div>
+        <AnimationVideo
+          src={section.content}
+          title="Youtube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    );
+  }
+  return null;
+}
+
+function App() {
+  const [page, setPage] = useState('No page is loaded yet!');
+  const [loading, setLoading] = useState(false);
+  const [sections, getSections] = useState([]);
+
+  const getPage = () => {
     setLoading(true);
-    api.getAllCourses()
+    api.getPage('correct_page_id')
       .then((res) => {
-        setCoursesList(res);
+        setPage(res);
         setLoading(false);
+        getSections(res.sections);
       });
   };
 
+  // const sectionList = page.sections;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <button
-          disabled={loading}
-          type="button"
-          onClick={() => getCoursesList()}
-        >
-          Get Courses
-        </button>
-        <p>{JSON.stringify(courses)}</p>
-      </header>
+      <button
+        disabled={loading}
+        type="button"
+        onClick={() => getPage()}
+      >
+        Get Learning Page
+      </button>
+      {sections.map((section) => sectionRendering(section))}
+      <p>{JSON.stringify(page)}</p>
+      {/* {sectionList.map((section) => <div><h1>{section.content}</h1></div>)} */}
     </div>
   );
 }
