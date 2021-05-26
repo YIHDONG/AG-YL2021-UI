bceafe26d199e0e7ef2a4538b98fc1c2debb38f1
YIHDONG <yihdong@student.unimelb.edu.au>
2021-05-26 21:14:04 +1000

    a problem fixed

--- src/Component/CourseNavigation/NavigationBar.jsx
+++ src/Component/CourseNavigation/NavigationBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classes from './NavigationBar.module.css';
 
-const NavigationBar = () => (
-  <div className={classes.NavigationBar}> </div>
-);
+const NavigationBar = ({ pageNumber }) => {
+  const dynamicHeight = `${30 * pageNumber}px`;
+
+  NavigationBar.propTypes = {
+    pageNumber: PropTypes.string.isRequired,
+  };
+
+  return (
+    <div className={classes.NavigationBar} style={{ height: dynamicHeight }}> </div>
+  );
+};
 
 export default NavigationBar;
