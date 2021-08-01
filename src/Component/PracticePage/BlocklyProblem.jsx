82a9a8e690f2fe39b31269ae98743545d80f60f8
maxplumley <maxplumley@users.noreply.github.com>
2021-08-02 00:13:07 +1000

    basic blocks used

--- src/Component/PracticePage/BlocklyProblem.jsx
+++ src/Component/PracticePage/BlocklyProblem.jsx
@@ -9,7 +9,7 @@ import BlocklyComponent, {
   Block, Value, Field, Shadow,
 } from '../Blockly';
 
-Blockly.Themes.Dark = Blockly.Theme.defineTheme('ag-theme', {
+Blockly.Theme.defineTheme('ag-theme', {
   base: Blockly.Themes.Classic,
   fontStyle: {
     family: 'Lato, sans-serif',
@@ -17,9 +17,80 @@ Blockly.Themes.Dark = Blockly.Theme.defineTheme('ag-theme', {
   },
 });
 
-Blockly.JavaScript.test_react_field = () => 'console.log(\'custom block\');\n';
+const forEachNodeBlockDef = {
+  type: 'for_each_graph_elem_in_graph',
+  message0: 'for each %1 %2 in %3',
+  args0: [
+    {
+      type: 'field_dropdown',
+      name: 'GRAPH_ELEM',
+      options: [
+        ['node', 'nodes'],
+        ['edge', 'edges'],
+      ],
+    },
+    { type: 'field_input', name: 'VAR_NAME' },
+    { type: 'input_value', name: 'GRAPH' },
+  ],
+  message1: 'do %1',
+  args1: [
+    { type: 'input_statement', name: 'DO' },
+  ],
+};
 
-Blockly.JavaScript.test_react_date_field = (block) => `console.log(${block.getField('DATE').getText()});\n`;
+Blockly.Blocks.for_each_graph_elem_in_graph = {
+  init() {
+    this.jsonInit(forEachNodeBlockDef);
+    this.setStyle('loop_blocks');
+  },
+};
+
+Blockly.JavaScript.for_each_graph_elem_in_graph = (block) => {
+  const graph = Blockly.JavaScript.statementToCode(block, 'GRAPH') || 'graph';
+  const graphElem = block.getFieldValue('GRAPH_ELEM') || 'node';
+  const nodeVar = block.getFieldValue('VAR_NAME') || 'node';
+  const statement = Blockly.JavaScript.statementToCode(block, 'DO') || '';
+  return `${graph}.${graphElem}.forEach((${nodeVar}) => {\n${statement}\n});\n`;
+};
+
+const graphBlockDef = {
+  type: 'graph',
+  message0: 'graph',
+  output: null,
+};
+
+Blockly.Blocks.graph = {
+  init() {
+    this.jsonInit(graphBlockDef);
+    this.setStyle('loop_blocks');
+  },
+};
+
+Blockly.JavaScript.graph = () => 'graph';
+
+const visitNode = {
+  type: 'visit_node',
+  message0: 'visit node %1 in %2',
+  args0: [
+    { type: 'field_input', name: 'VAR_NAME' },
+    { type: 'input_value', name: 'GRAPH' },
+  ],
+  previousStatement: null,
+  nextStatement: null,
+};
+
+Blockly.Blocks.visit_node = {
+  init() {
+    this.jsonInit(visitNode);
+    this.setStyle('loop_blocks');
+  },
+};
+
+Blockly.JavaScript.visit_node = (block) => {
+  const graph = Blockly.JavaScript.statementToCode(block, 'GRAPH') || 'graph';
+  const nodeVar = block.getFieldValue('VAR_NAME') || 'node';
+  return `${graph}.visitNode(${nodeVar}.id);\n`;
+};
 
 const Workspace = styled.div`
   display: flex;
@@ -52,32 +123,15 @@ const BlocklyProblem = ({ onSubmissionDataChange }) => {
           drag: true,
           wheel: true,
         }}
-        initialXml={`
-<xml xmlns="http://www.w3.org/1999/xhtml">
-<block type="controls_ifelse" x="0" y="0"></block>
-</xml>
-      `}
       >
-        <Block type="controls_ifelse" />
-        <Block type="logic_compare" />
-        <Block type="logic_operation" />
+        <Block type="for_each_graph_elem_in_graph" />
+        <Block type="graph" />
+        <Block type="visit_node" />
         <Block type="controls_repeat_ext">
           <Value name="TIMES">
             <Shadow type="math_number">
               <Field name="NUM">10</Field>
             </Shadow>
-          </Value>
-        </Block>
-        <Block type="logic_operation" />
-        <Block type="logic_negate" />
-        <Block type="logic_boolean" />
-        <Block type="logic_null" disabled="true" />
-        <Block type="logic_ternary" />
-        <Block type="text_charAt">
-          <Value name="VALUE">
-            <Block type="variables_get">
-              <Field name="VAR">text</Field>
-            </Block>
           </Value>
         </Block>
       </BlocklyComponent>
