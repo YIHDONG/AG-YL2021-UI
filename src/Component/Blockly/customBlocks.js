250448f6144e43f8da65a27f068e6448cb40160c
maxplumley <maxplumley@users.noreply.github.com>
2021-08-29 15:47:28 +1000

    SWEN900132021AG-191

--- src/Component/Blockly/customBlocks.js
+++ src/Component/Blockly/customBlocks.js
@@ -1,96 +1,105 @@
 import * as Blockly from 'blockly/core';
 
-const forEachNodeBlockDef = {
-  type: 'for_each_graph_elem_in_graph',
-  message0: 'for each %1 %2 in %3',
-  args0: [
-    {
-      type: 'field_dropdown',
-      name: 'GRAPH_ELEM',
-      options: [
-        ['node', 'nodes'],
-        ['edge', 'edges'],
-      ],
+const registerCustom = ({
+  id, definition, generator, style,
+}) => {
+  Blockly.Blocks[id] = {
+    init() {
+      this.jsonInit({ ...definition, type: id });
+      this.setStyle(style);
     },
-    { type: 'field_input', name: 'VAR_NAME' },
-    { type: 'input_value', name: 'GRAPH' },
-  ],
-  message1: 'do %1',
-  args1: [
-    { type: 'input_statement', name: 'DO' },
-  ],
+  };
+  Blockly.JavaScript[id] = generator;
 };
 
-Blockly.Blocks.for_each_graph_elem_in_graph = {
-  init() {
-    this.jsonInit(forEachNodeBlockDef);
-    this.setStyle('loop_blocks');
+registerCustom({
+  id: 'for_each_graph_elem_in_graph',
+  definition: {
+    message0: 'for each %1 %2 in %3',
+    args0: [
+      {
+        type: 'field_dropdown',
+        name: 'GRAPH_ELEM',
+        options: [
+          ['node', 'nodes'],
+          ['edge', 'edges'],
+        ],
+      },
+      { type: 'field_input', name: 'VAR_NAME' },
+      { type: 'input_value', name: 'GRAPH' },
+    ],
+    message1: 'do %1',
+    args1: [
+      { type: 'input_statement', name: 'DO' },
+    ],
   },
-};
-
-Blockly.JavaScript.for_each_graph_elem_in_graph = (block) => {
-  const graph = Blockly.JavaScript.statementToCode(block, 'GRAPH') || 'graph';
-  const graphElem = block.getFieldValue('GRAPH_ELEM') || 'node';
-  const nodeVar = block.getFieldValue('VAR_NAME') || 'node';
-  const statement = Blockly.JavaScript.statementToCode(block, 'DO') || '';
-  return `${graph}.${graphElem}.forEach((${nodeVar}) => {\n${statement}\n});\n`;
-};
-
-const graphBlockDef = {
-  type: 'graph',
-  message0: 'graph',
-  output: null,
-};
-
-Blockly.Blocks.graph = {
-  init() {
-    this.jsonInit(graphBlockDef);
-    this.setStyle('loop_blocks');
+  style: 'loop_blocks',
+  generator: (block) => {
+    const graph = Blockly.JavaScript.statementToCode(block, 'GRAPH') || 'graph';
+    const graphElem = block.getFieldValue('GRAPH_ELEM') || 'node';
+    const nodeVar = block.getFieldValue('VAR_NAME') || 'node';
+    const statement = Blockly.JavaScript.statementToCode(block, 'DO') || '';
+    return `${graph}.${graphElem}.forEach((${nodeVar}) => {\n${statement}\n});\n`;
   },
-};
+});
 
-Blockly.JavaScript.graph = () => 'executedGraph';
-
-const visitNode = {
-  type: 'visit_node',
-  message0: 'visit node %1',
-  args0: [
-    { type: 'field_input', name: 'VAR_NAME' },
-  ],
-  previousStatement: null,
-  nextStatement: null,
-};
-
-Blockly.Blocks.visit_node = {
-  init() {
-    this.jsonInit(visitNode);
-    this.setStyle('loop_blocks');
+registerCustom({
+  id: 'graph',
+  definition: {
+    message0: 'graph',
+    output: null,
   },
-};
+  style: 'loop_blocks',
+  generator: () => 'execution.variables.graph',
+});
 
-Blockly.JavaScript.visit_node = (block) => {
-  const nodeVar = block.getFieldValue('VAR_NAME') || 'node';
-  return `${nodeVar}.visit();\n`;
-};
-
-const traverseEdge = {
-  type: 'traverse_edge',
-  message0: 'traverse edge %1',
-  args0: [
-    { type: 'field_input', name: 'VAR_NAME' },
-  ],
-  previousStatement: null,
-  nextStatement: null,
-};
-
-Blockly.Blocks.traverse_edge = {
-  init() {
-    this.jsonInit(traverseEdge);
-    this.setStyle('loop_blocks');
+registerCustom({
+  id: 'visit_node',
+  definition: {
+    message0: 'visit node %1',
+    args0: [
+      { type: 'field_input', name: 'VAR_NAME' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
   },
-};
+  style: 'loop_blocks',
+  generator: (block) => {
+    const nodeVar = block.getFieldValue('VAR_NAME') || 'node';
+    return `${nodeVar}.visit();\n`;
+  },
+});
 
-Blockly.JavaScript.traverse_edge = (block) => {
-  const edgeVar = block.getFieldValue('VAR_NAME') || 'edge';
-  return `${edgeVar}.traverse();\n`;
-};
+registerCustom({
+  id: 'traverse_edge',
+  definition: {
+    message0: 'traverse edge %1',
+    args0: [
+      { type: 'field_input', name: 'VAR_NAME' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+  },
+  style: 'loop_blocks',
+  generator: (block) => {
+    const edgeVar = block.getFieldValue('VAR_NAME') || 'edge';
+    return `${edgeVar}.traverse();\n`;
+  },
+});
+
+registerCustom({
+  id: 'print_message',
+  definition: {
+    message0: 'print %1',
+    args0: [
+      { type: 'field_input', name: 'VAR_NAME' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+  },
+  style: 'loop_blocks',
+  generator: (block) => {
+    const value = block.getFieldValue('VAR_NAME') || '';
+    return `execution.console.push(${value})`;
+  },
+});
