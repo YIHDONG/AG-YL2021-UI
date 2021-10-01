607f25df49237fe3af38fd0e973350462a4384d4
maxplumley <maxplumley@users.noreply.github.com>
2021-10-01 10:46:28 +1000

    custom blocks

--- src/Component/Blockly/customBlocks.js
+++ src/Component/Blockly/customBlocks.js
@@ -103,3 +103,113 @@ registerCustom({
     return `execution.console.push(${value})`;
   },
 });
+
+registerCustom({
+  id: 'x_is_in_xs',
+  definition: {
+    message0: '%1 is in %2',
+    args0: [
+      { type: 'field_input', name: 'ITEM' },
+      { type: 'field_input', name: 'ITEMS' },
+    ],
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: (block) => {
+    const item = Blockly.JavaScript.statementToCode(block, 'ITEM') || '';
+    const items = Blockly.JavaScript.statementToCode(block, 'ITEMS') || [];
+    return `${items}.filter((i) => i == ${item}).length != 0`;
+  },
+});
+
+registerCustom({
+  id: 'x_is_y',
+  definition: {
+    message0: '%1 is %2',
+    args0: [
+      { type: 'field_input', name: 'ONE' },
+      { type: 'field_input', name: 'OTHER' },
+    ],
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: (block) => {
+    const one = Blockly.JavaScript.statementToCode(block, 'ONE') || '';
+    const other = Blockly.JavaScript.statementToCode(block, 'OTHER') || '';
+    return `${one} === ${other}`;
+  },
+});
+
+registerCustom({
+  id: 'neighbors_of_x',
+  definition: {
+    message0: 'neighbors of %1',
+    args0: [
+      { type: 'field_input', name: 'NODE' },
+    ],
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: (block) => {
+    const one = Blockly.JavaScript.statementToCode(block, 'NODE') || '';
+    return `${one}.edges.map((e) => e.toNode).filter((e) => e === ${one})`;
+  },
+});
+
+registerCustom({
+  id: 'visited_nodes',
+  definition: {
+    message0: 'visited nodes',
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: () => 'execution.variables.graph.nodes.filter((n) => n.visited)',
+});
+
+registerCustom({
+  id: 'unvisited_nodes',
+  definition: {
+    message0: 'unvisited nodes',
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: () => 'execution.variables.graph.nodes.filter((n) => !n.visited)',
+});
+
+registerCustom({
+  id: 'nodes',
+  definition: {
+    message0: 'nodes',
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: () => 'execution.variables.graph.nodes',
+});
+
+registerCustom({
+  id: 'edges',
+  definition: {
+    message0: 'edges',
+  },
+  style: 'loop_blocks',
+  generator: () => 'execution.variables.graph.edges',
+});
+
+registerCustom({
+  id: 'edge_from_x_to_y',
+  definition: {
+    message0: 'edge from %1 to %2',
+    args0: [
+      { type: 'input_value', name: 'FROM' },
+      { type: 'input_value', name: 'TO' },
+    ],
+    inputsInline: true,
+    output: null,
+  },
+  style: 'loop_blocks',
+  generator: (block) => {
+    const from = Blockly.JavaScript.statementToCode(block, 'FROM') || null;
+    const to = Blockly.JavaScript.statementToCode(block, 'TO') || null;
+    return `execution.variables.graph.edges.find((e) -> e.fromNode === ${from} && e.toNode === ${to}`;
+  },
+});
